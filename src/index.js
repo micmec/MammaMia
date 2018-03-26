3fd2b56ae19017c2b8e6f549bbf158bbcb7e2e4d
micmec <mm2275@bath.ac.uk>
2018-03-26 19:15:31 +0200

    Restructure of directories for client/server division

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import Calculator from './components/Calculator';
-import SignUp from './components/SignUp';
+import App from '../client/components/App';
+import Calculator from '../client/components/Calculator';
+import SignUpPage from '../client/components/SignUpPage';
 import {BrowserRouter as Router, Route,} from "react-router-dom";
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from '../client/registerServiceWorker';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux'
+
+
+//TODO reinvent logic using routing
+//TODO remove HTML5up and use semantic-ui
 
 class Main extends React.Component {
     render() {
         return <Router>
             <div>
                 <Route exact path="/" component={App}/>
-                <Route path="/signup" component={SignUp}/>
-
+                <Route path="/signup" component={SignUpPage}/>
                 <Route path="/calculator" component={Calculator}/>
             </div>
         </Router>
     }
 }
 
-ReactDOM.render(<Main/>, document.getElementById('root'));
+const store = createStore(
+    (state = {}) => state,
+    applyMiddleware(thunk)
+)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Main/>
+    </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
